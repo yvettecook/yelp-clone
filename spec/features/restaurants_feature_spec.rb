2da88ed7c9290014f677c8b24e90db2e0b47a5a7
yvettecook <yvettecook@gmail.com>
2014-11-03 11:51:59 +0000

    List of restaurants added to restaurant views

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -12,4 +12,18 @@ describe 'restaurants' do
 
 	end
 
+	context 'restaurants have been added' do
+
+		before do
+			Restaurant.create(name: 'KFC')
+		end
+
+		it 'should display restaurants' do
+			visit '/restaurants'
+			expect(page).to have_content 'KFC'
+			expect(page).not_to have_content 'No restaurants yet'
+		end
+
+	end
+
 end
