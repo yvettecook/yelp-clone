79641dd3454e5dd440fbe8145f647b8a5895e757
yvettecook <yvettecook@gmail.com>
2014-11-04 12:29:16 +0000

    Added user model and tests using Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :restaurants do
     resources :reviews
   end
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'restaurants#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Review, :type => :model do
     expect(review).to have(1).error_on(:rating)
   end
 
+  it 'is invalid if the rating is less than 1' do
+    review = Review.new(rating: -1)
+    expect(review).to have(1).error_on(:rating)
+  end
+
+
+    it 'is valid if the rating is between 1 and 5' do
+      review = Review.new(rating: 3)
+      expect(review).to have(0).error_on(:rating)
+    end
+
 end
