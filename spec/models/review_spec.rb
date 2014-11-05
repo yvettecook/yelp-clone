5a741504aab638d9214a6eb8b3e36eff2f2304fb
yvettecook <yvettecook@gmail.com>
2014-11-05 17:11:33 +0000

    partial restaurant form added

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -34,6 +34,10 @@ RSpec.describe Review, :type => :model do
       expect(review2).to have(1).error
     end
 
+    it 'can have 2 anonymous reviews per restaurant' do
+      
+    end
+
   end
 
 end
